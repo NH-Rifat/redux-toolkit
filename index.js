39b5b3e35c1ae51dcfdfcbde774ad0b70fa55a12
NH-Rifat <rifatr729@gmail.com>
2022-06-23 05:15:23 +0600

    payload added

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 const { createStore } = require('redux');
 
+// constant........
 const Increment = 'Increment';
 const Decrement = 'Decrement';
+const Increment_By_Value = 'Increment_By_Value'
+const Add_User = 'Add_user'
 
 const initialCounterState = {
   count: 0,
+  users : ['Anis','Kanis'],
 };
 
+// action---Object- type, payload........
 const incrementCounter = () => {
   return {
     type: Increment,
   };
 };
+
 const decrementCounter = () => {
   return {
     type: Decrement,
   };
 };
 
+const incrementByValue = (value) => {
+  return {
+    type:Increment_By_Value,
+    payload:value
+  };
+};
+
+const addUser = (user)=>{
+  return{
+    type:Add_User,
+    payload:user,
+  }
+}
+
+
+// create reducer for counter.......
 const counterReducer = (state = initialCounterState, action) => {
   switch (action.type) {
     case Increment:
@@ -28,6 +50,17 @@ const counterReducer = (state = initialCounterState, action) => {
     case Decrement:
       return {
         count: state.count - 1,
+      };
+
+    case Increment_By_Value:
+      return {
+        count: state.count + action.payload,
+      };
+
+    case Add_User:
+      return {
+        users:[...state.users,action.payload],
+        // count: state.count + action.payload,
       };
 
     default:
@@ -42,9 +75,13 @@ store.subscribe(()=>{
   console.log(store.getState());
 })
 
-store.dispatch(incrementCounter());
-store.dispatch(incrementCounter());
-store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
 
-store.dispatch(decrementCounter());
-store.dispatch(decrementCounter());
+// store.dispatch(decrementCounter());
+// store.dispatch(decrementCounter());
+
+// store.dispatch(incrementByValue(5))
+
+store.dispatch(addUser('Rahim'))
